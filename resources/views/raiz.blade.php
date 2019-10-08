014a572763a283cefef340abc8a7b46b733740c7
Matheus Vinicius <matheus2vandrade@gmail.comgit config --global user.email matheus2vandrade@gmail.com>
2019-10-08 10:40:30 -0300

    raizstart

---
+++ resources/views/raiz.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Events</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!--  Styles  -->
+          <link rel="stylesheet" href="css/processed.css">
+          <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <br>
+    <nav>
+        <a style="font-family: raleway" href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Casamentos</a>
+        <a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Aniversários</a>
+        <a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Corporativos</a>
+        <a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Planos</a>
+        <a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contatos</a>
+        <a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sobre</a>
+    </nav>
+  <header class="p-5">
+    <div style="font-family: raleway" class="row m-0 p-0">
+     
+    <div class="col-lg-6 col-md-6">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="images/casamentos.jpeg" height="400px" width="400px" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="images/corporativos.jpg" height="400px" width="400px" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="images/niver.jpeg" height="400px" width="400px" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+            </div>
+    </div> 
+
+ <div class="col-lg-6 col-md-6" id="inicio">
+      <h1 class="text-dark">Events</h1>
+      <br>
+      <p class="text-dark">O seu presente da maneira mais cômoda possível, o seu convidado compra um presente no nosso site e você recebem em dinheiro. Seja para qualquer tipo de evento, basta fazer o cadastro e fazer parte da nossa rede.
+      <br>
+      <br>
+      Aqui você pode ter a confirmação de todos os que irão estar presentes em seu evento, assim como acesso aos presentes recebidos, com a vantagem de seu convidado poder, passar o cartão ou boleto bancário e te presentear da melhor forma.
+      <br>
+      <br>
+      O Events foi criado com o intuito de acabar com o constragimento de pedir dinheiro ao seu convidado para que contribuir com sua festa, nós transformando o presente em dinheiro. 
+      <br>
+      <br>
+      Para maiores informações, entre em contato conosco
+      </p>
+      <br>
+ </div>
+</div> <!--row-->
+    
+  </header>
+
+
+  <!-- Scripts -->
+  <script src="scripts/index.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
